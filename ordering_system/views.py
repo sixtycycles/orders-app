d754dccd8cd9a696afc433d7fc5f9a953955fa62
rod <sixtycycles@gmail.com>
2021-12-23 15:36:18 -0800

    add login and required decorators

--- ordering_system/views.py
+++ ordering_system/views.py
@@ -4,7 +4,7 @@ from django.forms.models import BaseModelForm
 from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseRedirect
 from django.http.request import HttpRequest
 from django.shortcuts import get_object_or_404, render, redirect
-
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.views.generic import CreateView, DetailView, ListView
 from .forms import ItemForm, OrderForm
@@ -14,7 +14,7 @@ from .models import Item, Order
 def landing_page(request):
     return render(request, "ordering_system/landing.html")
 
-
+@login_required
 def order_list_view(request):
     context = {}
     context["list_of_orders"] = Order.objects.filter(user=request.user)
@@ -22,7 +22,7 @@ def order_list_view(request):
 
     return render(request, "ordering_system/order_list.html", context)
 
-
+@login_required
 def order_create_view(request):
     context = {}
     form = OrderForm(request.POST or None)
@@ -36,14 +36,14 @@ def order_create_view(request):
     context["form"] = form
     return render(request, "ordering_system/order_create_form.html", context)
 
-
+@login_required
 def order_detail_view(request, pk):
     context = {}
     context["order"] = get_object_or_404(Order, pk=pk)
 
     return render(request, "ordering_system/order_detail.html", context)
 
-
+@login_required
 def order_add_item_view(request, pk):
     context = {}
     order = get_object_or_404(Order, pk=pk)
@@ -54,7 +54,7 @@ def order_add_item_view(request, pk):
     context["Order"] = Order
     return render(request, "ordering_system/order_items.html", context)
 
-
+@login_required
 def order_edit_view(request, pk):
     context = {}
     edit_this = get_object_or_404(Order, pk=pk)
@@ -69,7 +69,7 @@ def order_edit_view(request, pk):
 
     return render(request, "ordering_system/order_edit.html", context)
 
-
+@login_required
 def order_delete_view(request, pk):
     context = {}
     obj = get_object_or_404(Order, pk=pk)
@@ -84,7 +84,7 @@ def order_delete_view(request, pk):
         ]
     )
 
-
+@login_required
 def item_create_view(request, pk):
     context = {}
     order = get_object_or_404(Order, pk=pk)
@@ -102,7 +102,7 @@ def item_create_view(request, pk):
     context["order_id"] = pk
     return render(request, "ordering_system/item_create_form.html", context)
 
-
+@login_required
 def item_edit_view(request, pk):
     context = {}
     item = get_object_or_404(Item, pk=pk)
@@ -116,7 +116,7 @@ def item_edit_view(request, pk):
     context["form"] = form
     return render(request, "ordering_system/item_edit_form.html", context)
 
-
+@login_required
 def item_detail_view(request, pk):
     context = {}
     item = get_object_or_404(Item, pk=pk)
@@ -124,7 +124,7 @@ def item_detail_view(request, pk):
 
     return render(request, "ordering_system/item_detail.html", context)
 
-
+@login_required
 def item_delete_view(request, pk):
     context = {}
     obj = get_object_or_404(Item, pk=pk)
